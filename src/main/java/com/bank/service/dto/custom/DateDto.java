c87052327074958accc02492b9bb5879fdc7cb36
qinxinddv <79964674@qq.com>
2020-02-29 22:24:02 +0800

    bug

--- src/main/java/com/bank/service/dto/custom/DateDto.java
+++ src/main/java/com/bank/service/dto/custom/DateDto.java
@@ -1,5 +1,7 @@
 package com.bank.service.dto.custom;
 
+import java.util.Objects;
+
 public class DateDto {
     private String value;
 
@@ -16,5 +18,19 @@ public class DateDto {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DateDto dateDto = (DateDto) o;
+        return Objects.equals(value, dateDto.value);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(value);
     }
 }
